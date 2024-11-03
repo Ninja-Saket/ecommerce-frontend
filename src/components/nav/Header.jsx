088c8f2739b78f654f8e7dda9873b7e4a41e273f
Saket Ray <phaser.84059@gmail.com>
2024-11-03 08:44:46 +0530

    Added register form and setup firebase for authentication

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Menu, Space } from "antd";
+import { App, Menu, Space } from "antd";
 import { Link } from "react-router-dom";
 import {
   AppstoreOutlined,
@@ -9,6 +9,42 @@ import {
 } from "@ant-design/icons";
 const { SubMenu, Item } = Menu;
 
+const itemsLeft = [
+    {
+        label: <Link to='/'>Home</Link>,
+        key : 'home',
+        icon : <AppstoreOutlined/>
+    },
+    {
+        label : 'Username',
+        key : 'username',
+        icon : <SettingOutlined/>,
+        children : [
+            {
+                label : 'Option 1',
+                key : 'option1'
+            },
+            {
+                label : 'Option 2',
+                key : 'option2'
+            }
+        ]
+    }
+]
+
+const itemsRight = [
+    {
+        label : <Link to='/login'>Login</Link>,
+        key : 'login',
+        icon : <UserOutlined/>
+    },
+    {
+        label : <Link to='/register'>Register</Link>,
+        key : 'register',
+        icon : <UserAddOutlined/>
+    }
+]
+
 const Header = () => {
   const [current, setCurrent] = useState("home");
   const onClick = (e) => {
@@ -16,22 +52,12 @@ const Header = () => {
     setCurrent(e.key);
   };
   return (
-    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
-      <Item key="home" icon={<AppstoreOutlined />}>
-        <Link to="/">Home</Link>
-      </Item>
-      <SubMenu icon={<SettingOutlined />} title="Username">
-        <Item key="option1">Option 1</Item>
-        <Item key="option2">Option 2</Item>
-      </SubMenu>
-
-      <Item key="login" icon={<UserOutlined />} style={{ marginLeft: "auto" }}>
-        <Link to="/login">Login</Link>
-      </Item>
-      <Item key="register" icon={<UserAddOutlined />}>
-        <Link to="/register">Register</Link>
-      </Item>
-    </Menu>
+    <div className="container-fluid mx-0 p-0">
+        <div className="row mx-0">
+            <div className="col-6"><Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" key="navmenuleft" items={itemsLeft}/></div>
+            <div className="col-6 d-flex justify-content-end"><Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" key="navmenuright" items={itemsRight}/></div>
+        </div>
+    </div>
   );
 };
 
