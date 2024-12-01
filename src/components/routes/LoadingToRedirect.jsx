025cd7dd282edd03d53233f9041a3beba86feb72
Saket Ray <phaser.84059@gmail.com>
2024-12-01 14:24:02 +0530

    Added redirection countdown

---
+++ src/components/routes/LoadingToRedirect.jsx
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from "react"
+import { useLocation, Navigate, useNavigate } from "react-router-dom"
+
+const LoadingToRedirect = () => {
+    const [count, setCount] = useState(5)
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    useEffect(()=> {
+        const interval = setInterval(()=> {
+            setCount((currentCount) => --currentCount)
+        }, 1000)
+        // Redirect to login once count is 0
+        if (count === 0) {
+            navigate("/login", { replace: true, state: { from: location } });
+        }
+        // Cleanup
+        return ()=> clearInterval(interval)
+    },[count])
+
+    return (
+        <div className="container p-5 text-center">
+            <p>Redirecting to Login in {count} seconds</p>
+        </div>
+    )
+}
+
+export default LoadingToRedirect
--- src/components/routes/UserRoute.jsx
+++ src/components/routes/UserRoute.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useLocation, Navigate, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import LoadingToRedirect from './LoadingToRedirect'
 
 const UserRoute = () => {
-    const location = useLocation()
     const user = useSelector((state) => state.user)
-    return user && user.token ? <Outlet/> : <Navigate to="/login" replace state={{from : location}}/>
+    return user && user.token ? <Outlet/> : <LoadingToRedirect/>
 }
 
 export default UserRoute;
