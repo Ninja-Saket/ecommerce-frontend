27ea7756039c39db6eca9e97aa2b5d84314e3383
Saket Ray <phaser.84059@gmail.com>
2024-12-17 12:34:08 +0530

    Added file upload feature while creating product

---
+++ src/components/forms/FileUpload.jsx
@@ -0,0 +1,83 @@
+import React from "react"
+import Resizer from "react-image-file-resizer"
+import axios from 'axios'
+import { useSelector } from "react-redux"
+import { toast } from "react-toastify"
+import { Badge, Avatar, Space } from "antd"
+
+const FileUpload = ({values, setValues, setLoading}) => {
+    const userToken = useSelector(state => state.user.token)
+    const fileUploadAndResize = (e)=> {
+        let files = e.target.files
+        let allUploadedFiles = values.images
+        if(files){
+            setLoading(true)
+            let noerror = true;
+            for(let i = 0; i < files.length; i++){
+                Resizer.imageFileResizer(files[i], 720, 720, 'JPEG', 100, 0, (uri)=> {
+                    axios.post(`${import.meta.env.VITE_APP_API}/uploadimages`, {image : uri}, {
+                        headers: {
+                            authToken : userToken
+                        }
+                    }).then(result => {
+                        setLoading(false)
+                        allUploadedFiles.push(result.data)
+                        setValues({...values, images : allUploadedFiles})
+                    }).catch(err => {
+                        noerror = false
+                        setLoading(false)
+                        console.log('Cloudinary upload error : ', err)
+                        toast.error('Cloudinary upload error')
+                    })
+                }, 'base64')
+            }
+        }
+    }
+
+    const handleImageRemove = (public_id) => {
+        setLoading(true)
+        console.log('Remove image : ', public_id)
+        axios.post(`${import.meta.env.VITE_APP_API}/removeimage`, {public_id},{
+            headers : {
+                authToken : userToken
+            }
+        }).then(res => {
+            setLoading(false)
+            const {images} = values;
+            let filteredImages = images.filter((item)=> {
+                return item.public_id !== public_id
+            })
+            setValues({...values, images: filteredImages})
+        }).catch(err => {
+            console.log(err)
+            setLoading(false)
+        })
+    }
+
+    return (
+        <>
+            <div className="row">
+                {values.images && values.images.map((image) => (
+                    <Space size="middle" className="col-md-1 px-0 mx-3 col-sm-2" key={image.public_id}>
+                        <Badge count="X" className="px-0 mx-0" onClick={()=> handleImageRemove(image.public_id)} style={{cursor: 'pointer'}}>
+                            <Avatar
+                            src={image.url}
+                            size={100}
+                            shape="square"
+                            className="px-0 mx-0"
+                            />
+                        </Badge>
+                    </Space>
+                ))}
+            </div>
+            <div className="row">
+                <label className="col-md-2 mx-2 my-2 btn btn-info">Choose File
+                    <input type="file" multiple accept="images/*" onChange={fileUploadAndResize} hidden/>
+                </label>
+            </div>
+        </>
+        
+    )
+}
+
+export default FileUpload
--- src/pages/admin/product/CreateProduct.jsx
+++ src/pages/admin/product/CreateProduct.jsx
@@ -4,8 +4,11 @@ import { createProduct } from "../../../apiCalls/product";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import CreateProductForm from "../../../components/forms/CreateProductForm";
-import { getCategories, getCategorySubCategories } from "../../../apiCalls/category"
-
+import {
+  getCategories,
+  getCategorySubCategories,
+} from "../../../apiCalls/category";
+import FileUpload from "../../../components/forms/FileUpload";
 
 const initialState = {
   title: "",
@@ -25,59 +28,59 @@ const initialState = {
 
 const CreateProduct = () => {
   const [values, setValues] = useState(initialState);
-  const [loading, setLoading] = useState(false)
-  const [subCategoryOptions, setSubCategoryOptions] = useState([])
-  const [showSubCategories, setShowSubCategories] = useState(false)
-  const userToken = useSelector((state) => state.user.token)
+  const [loading, setLoading] = useState(false);
+  const [subCategoryOptions, setSubCategoryOptions] = useState([]);
+  const [showSubCategories, setShowSubCategories] = useState(false);
+  const userToken = useSelector((state) => state.user.token);
 
-  const loadCategories = async ()=> {
-    try{
-        const result = await getCategories()
-        setValues({...values, categories : result.data})
-    }catch(err){
-        toast.error('Load Categories failed in create product')
+  const loadCategories = async () => {
+    try {
+      const result = await getCategories();
+      setValues({ ...values, categories: result.data });
+    } catch (err) {
+      toast.error("Load Categories failed in create product");
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    setLoading(true)
-    try{
-        const result = await createProduct(values, userToken)
-        setLoading(false)
-        toast.success(`Product "${values.title}" created successfully`)
-        setValues(initialState)
-        setShowSubCategories(false)
-    }catch(err){
-        setLoading(false)
-        if(err.response.status === 400){
-            toast.error(err.response.data.err)
-        }
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const result = await createProduct(values, userToken);
+      setLoading(false);
+      toast.success(`Product "${values.title}" created successfully`);
+      setValues({...initialState, images: []});
+      setShowSubCategories(false);
+    } catch (err) {
+      setLoading(false);
+      if (err.response.status === 400) {
+        toast.error(err.response.data.err);
+      }
     }
   };
 
   const handleChange = (e) => {
-    setValues({...values, [e.target.name] : e.target.value})
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const handleCategoryChange = async (e) => {
-    e.preventDefault()
-    setValues({...values, subCategories: [], category : e.target.value})
-    try{
-        const result = await getCategorySubCategories(e.target.value)
-        setSubCategoryOptions(result.data)
-        setShowSubCategories(true)
-    }catch(err){
-        setShowSubCategories(false)
-        if(err.response.status === 400){
-            toast.error(err.response.data)
-        }
+    e.preventDefault();
+    setValues({ ...values, subCategories: [], category: e.target.value });
+    try {
+      const result = await getCategorySubCategories(e.target.value);
+      setSubCategoryOptions(result.data);
+      setShowSubCategories(true);
+    } catch (err) {
+      setShowSubCategories(false);
+      if (err.response.status === 400) {
+        toast.error(err.response.data);
+      }
     }
-  }
+  };
 
-  useEffect(()=> {
-    loadCategories()
-  },[])
+  useEffect(() => {
+    loadCategories();
+  }, []);
   return (
     <div className="container-fluid">
       <div className="row">
@@ -85,8 +88,21 @@ const CreateProduct = () => {
           <AdminNav />
         </div>
         <div className="col-md-10">
-        {loading ? (<h4 className="text-danger">Loading....</h4>) : (<h4>Create Product</h4>)}
-          <CreateProductForm handleChange={handleChange} handleSubmit={handleSubmit} handleCategoryChange={handleCategoryChange} values={values} subCategoryOptions={subCategoryOptions} showSubCategories={showSubCategories} setValues={setValues}/>
+          {loading ? (
+            <h4 className="text-danger">Loading....</h4>
+          ) : (
+            <h4>Create Product</h4>
+          )}
+          <FileUpload values={values} setValues={setValues} setLoading={setLoading}/>
+          <CreateProductForm
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            handleCategoryChange={handleCategoryChange}
+            values={values}
+            subCategoryOptions={subCategoryOptions}
+            showSubCategories={showSubCategories}
+            setValues={setValues}
+          />
         </div>
       </div>
     </div>
