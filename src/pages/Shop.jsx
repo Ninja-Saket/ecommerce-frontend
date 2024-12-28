6ed63a9d3bbb373737893eec9452658bdb7db43e
Saket Ray <phaser.84059@gmail.com>
2024-12-28 12:49:21 +0530

    Added category filter for products

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,18 +1,43 @@
 import React, {useState, useEffect, Children} from 'react'
 import {getProducts, getProductsByFilters} from '../apiCalls/product'
+import {getCategories} from '../apiCalls/category'
 import {useSelector, useDispatch} from 'react-redux'
 import ProductCard from '../components/cards/ProductCard'
-import {Menu, Slider} from 'antd'
-import {DollarOutlined} from '@ant-design/icons'
+import {Menu, Slider, Checkbox} from 'antd'
+import {DollarOutlined, DownSquareOutlined} from '@ant-design/icons'
+
 
 const Shop = () => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(false)
     const [price, setPrice] = useState([0,0])
-    const [ok, setOk] = useState(false)
+    const [categories, setCategories] = useState([])
+    const [menuItems, setMenuItems] = useState([])
+    const [categoryIds, setCategoryIds] = useState([])
     const dispatch = useDispatch()
     const search = useSelector((state)=> state.search)
     const {text} = search
+
+    const handleCheck = async (e)=> {
+        try{
+            dispatch({type : 'SEARCH_QUERY',
+                payload : {text : ''}
+            })
+            setPrice([0,0])
+            const categoryIdsInState = [...categoryIds]
+            const currentCategory = e.target.value
+            const currentCategoryIndex = categoryIdsInState.indexOf(currentCategory)
+            if(currentCategoryIndex === -1){
+                categoryIdsInState.push(currentCategory)
+            }else{
+                categoryIdsInState.splice(currentCategoryIndex,1)
+            }
+            setCategoryIds(categoryIdsInState)
+            await loadProductsByFilter({category : categoryIdsInState})
+        }catch(err){
+            console.log(err)
+        }
+    }
 
     const handleSlider = async (value)=> {
         dispatch({
@@ -21,28 +46,26 @@ const Shop = () => {
                 text : ""
             }
         })
+        setCategoryIds([])
         setPrice(value)
     }
 
-    const items = [
-        {
-            key : '1',
-            label : 'Price',
-            icon : <DollarOutlined/>,
-            children : [
-                {
-                    key : 'priceRange',
-                    label : <Slider className='custom-slider' tooltip={{formatter : (value) => `$${value}`}} value={price} onChange={handleSlider} max="6999" range/>
-                }
-            ]
-        }
-    ]
+    const showCategories = () => {
+        return categories.map((c) => (
+            <Checkbox key={c._id} name='category' value={c._id} className='pb-2 pl-4 pr-4' onChange={handleCheck}
+                checked={categoryIds.includes(c._id)}
+            >{c.name}</Checkbox>
+        ))
+    }
 
     const loadProducts = async ()=> {
         try{
             setLoading(true)
             const result = await getProducts(12)
             setProducts(result.data)
+            const categoryResult = await getCategories()
+            console.log('Category result', categoryResult)
+            setCategories(categoryResult.data)
         }catch(err){
             console.log(err)
         }finally{
@@ -85,6 +108,35 @@ const Shop = () => {
         return () => clearTimeout(timer);
     }, [price])
 
+    useEffect(()=> {
+        const items = [
+            {
+                key : '1',
+                label : 'Price',
+                icon : <DollarOutlined/>,
+                children : [
+                    {
+                        key : 'priceRange',
+                        label : <Slider className='custom-slider' tooltip={{formatter : (value) => `$${value}`}} value={price} onChange={handleSlider} max="6999" range/>
+                    }
+                ]
+            },
+            {
+                key : '2',
+                label : 'Category',
+                icon : <DownSquareOutlined/>,
+                className: 'custom-category-item',
+                children : [
+                    {
+                        key : 'categories',
+                        label : <div className='custom-menu-categories '>{showCategories()}</div>
+                    }
+                ]
+            }
+        ]
+        setMenuItems(items)
+    }, [categories, price, categoryIds])
+
     return (
         <div className="container-fluid">
             <div className='row'>
@@ -92,7 +144,7 @@ const Shop = () => {
                     <h4>Search/Filter</h4>
                     <Menu mode='inline'
                         defaultOpenKeys={['1']}
-                        items={items}
+                        items={menuItems}
                         className='custom-menu'
                     />
                     
